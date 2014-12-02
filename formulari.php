052cebfbe58bd8d061291a37adad0c7d5da82635
catbru <catbru@gmail.com>
2014-12-02 18:49:26 +0100

    Create formulari.php

---
+++ formulari.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CercalAPI</title>
+    </head>
+    <body>
+        <img src="logo.png" width="164" height="92" alt="" align="bottom">
+        <img src="by-nc-sa.png" width="115" height="40" alt="" align="right">
+        
+        <p><strong>CercalAPI</strong> v.0.1</p>
+
+<p>
+Ompla el formulari. Si camp en blanc mort. Aix&ograve; &eacute;s Vars&iacute;ria, no &eacute;s Irak</p><br />
+        
+        
+        <form action="index.php" method="get">
+        <p>Cerca: (%22 = espai, %20 = cometes) <input type="text" name="query" /></p>
+        <p>Localització (coordenades i radi)</p>
+        <p>Latitud: <input type="text" name="lat" /></p>
+        <p>Longitud: <input type="text" name="lon" /></p>
+        <p>Radi (km): <input type="text" name="near" /></p>
+        <p>Nombre de tweets (max 100): <input type="text" name="count" /></p>
+         <p><input type="submit" /></p>
+        </form> 
+        
+      
+
+      
+        
+        <meta charset="utf-8">
+
+        
+        
+            </body>
+</html>
